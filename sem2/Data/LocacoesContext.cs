5918afee508082008e1c6bbcdd4f51e035ed0cf2
LMguel <miguelesquivel2018@outlook.com>
2024-08-26 20:51:00 -0300

    Aula dia 26/08

---
+++ sem2/Data/LocacoesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sem2.Models;
+
+namespace sem2.Data
+{
+    public class LocacoesContext : DbContext
+    {
+        public LocacoesContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
--- sem2/Program.cs
+++ sem2/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using sem2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("LocAutoConnection");
+builder.Services.AddDbContext<LocacoesContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
